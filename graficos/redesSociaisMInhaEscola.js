9fe3483b1fb8bc03dd9aa0070e30626dcadda181
starfelyne <117102298+starashley@users.noreply.github.com>
2024-10-26 00:20:51 -0300

    Add files via upload

---
+++ graficos/redesSociaisMInhaEscola.js
@@ -0,0 +1,60 @@
+import { criarGrafico, getCSS, incluirTexto } from "./common.js"
+
+async function redesSociaisFavoritasMinhaEscola() {
+    const dadosLocaisString = localStorage.getItem('respostaRedesSociais')
+    if (dadosLocaisString) {
+        const dadosLocais = JSON.parse(dadosLocaisString)
+        processarDados(dadosLocais)
+    } else {
+    const url = 'https://script.google.com/macros/s/AKfycbwwB_xTMLFYD-58l1cpm3z_8TQgxHA2ZF_Rr_xjkF3Gw_hp1mReThCUx041cQq3f9yI/exec'
+    const res = await fetch(url)
+    const dados = await res.json()
+    localStorage.setItem('respostaRedesSociais', JSON.stringify(dados))
+    processarDados(dados)
+}
+}
+
+function processarDados(dados) {
+const redesSociais = dados.slice(1).map(redes => redes[1])
+const contagemRedesSociais = redesSociais.reduce((acc, redesSociais) => {
+    acc[redesSociais] = (acc[redesSociais] || 0) + 1
+    return acc
+}, {})
+const valores = Object.values(contagemRedesSociais)
+const labels = Object.keys(contagemRedesSociais)
+
+const data = [
+    {
+        values: valores,
+        labels: labels,
+        type: 'pie',
+        textinfo: 'label+percent'
+    }
+]
+
+const layout = {
+    plot_bgcolor: getCSS('--bg-color'),
+    paper_bgcolor: getCSS('--bg-color'),
+    height: 700,
+    title: {
+        text: 'Redes sociais que as pessoas da minha escola mais gostam',
+        x: 0,
+        font: {
+            color: getCSS('--primary-color'),
+            family: getCSS('--font'),
+            size: 30
+        }
+    },
+    legend: {
+        font: {
+            color: getCSS('--primary-color'),
+            size: 16
+        }
+    }
+}
+
+criarGrafico(data, layout)
+incluirTexto(`Como no mundo, a amostra de pessoas entrevistadas por mim, demonstra um apreço pelo <span>TikTok</span> em relação a outras redes.`)
+}
+
+redesSociaisFavoritasMinhaEscola()
